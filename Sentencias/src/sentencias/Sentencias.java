4b1b78d0b89b662a6f0cbb009d227eeaa83d709a
JeO <jeo2k1@gmail.com>
2024-05-20 16:29:01 -0300

    Sentencias de control 1 (IF - ELSE, SWITCH)

---
+++ Sentencias/src/sentencias/Sentencias.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sentencias;
+
+/**
+ *
+ * @author m26344676
+ */
+public class Sentencias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     /* int a = 15;
+        int b = 26;
+        int c = 10;
+        
+        if(a > b){
+            if (a > c){
+                System.out.println("A es mayor");            
+            }else{
+                System.out.println("C es mayor");            
+            }            
+        } else if(b > c) {
+            System.out.println("B es mayor");                        
+        } else{
+            System.out.println("C es mayor");            
+        } */
+        char letra = 'O';
+        switch (letra){
+            
+            case 'M':
+                System.out.println("Letra M");            
+                break;
+                
+            case 'N':
+                System.out.println("Letra N");
+                break;
+                
+            case 'O':
+                System.out.println("Letra O");
+                break;                                
+                
+        }
+    }
+    
+}
