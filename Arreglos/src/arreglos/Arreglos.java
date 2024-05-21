61fdda549cf4cffd9c38aca0aed9f4ab5e3eddd6
JeO <jeo2k1@gmail.com>
2024-05-20 23:29:31 -0300

    5: Arreglos y Matrices (Arrays) - parcial

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arreglos;
+
+/**
+ *
+ * @author m26344676
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] numeros = new int[10];
+        for (int i=0; i<10; i++){
+            numeros[i] = i;
+            System.out.println(numeros[i]);
+        }
+
+    }
+    
+}
