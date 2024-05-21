12db10be72217ce9a8c7b5a091b9d1bf1a022150
JeO <jeo2k1@gmail.com>
2024-05-21 10:18:39 -0300

    5: Arreglos y Matrices (Arrays)

--- Arreglos/src/arreglos/Arreglos.java
+++ Arreglos/src/arreglos/Arreglos.java
@@ -9,10 +9,8 @@ package arreglos;
  * @author m26344676
  */
 public class Arreglos {
-
-    /**
-     * @param args the command line arguments
-     */
+    /* ARREGLOS */
+    /*     
     public static void main(String[] args) {
         int [] numeros = new int[10];
         for (int i=0; i<10; i++){
@@ -21,5 +19,25 @@ public class Arreglos {
         }
 
     }
-    
+    public static void main(String[] args) {
+        int[] datos = new int[10];
+        int resultado = 0;
+        for(int i = 0; i < 10; i++){
+            datos[i] = (int) (Math.random() * 9);
+            resultado = resultado + datos[i];
+        }
+        System.out.println("La suma de los valores es: "+resultado);
+    }*/
+    /* MATRICES */
+        public static void main(String[] args) {
+        int[][] numero = new int[4][4];
+        for(int i = 0; i < 4; i++){ /* for que recorre las filas de la matriz*/
+            for(int j = 0; j < 4; j++){/* for que recorre las columnas*/
+                numero[i][j] = (int)(Math.random() * 9);
+                System.out.print(numero[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
 }
